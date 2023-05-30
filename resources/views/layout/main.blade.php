46bc4a06cab190fc70e3b94facd56460b6a4bc3c
kelvinhenri99 <kelvin5henri@gmail.com>
2023-05-30 13:43:04 -0300

    feat: create layout system

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="../../css/welcome.css">
+	<link rel="stylesheet" href="../../css/LoginOrRegister.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Acme&family=Heebo:wght@300&family=Signika&family=Source+Serif+Pro&display=swap" rel="stylesheet">
+	<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+	<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+	<title>Chat Manager</title>
+</head>
+<body>
+	@yield('content')
+	<div class="version">
+		<p>Sistema: 1.0.0</p>
+		<p>Laravel v-10.12.0 | PHP v-8.2.0</p>
+	</div>
+</body>
+</html>
